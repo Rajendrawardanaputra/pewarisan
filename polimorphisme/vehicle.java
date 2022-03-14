c7b0a864ac6ae9d4d1d4db6fc7b6d56d3bcc4d70
Rajendrawardanaputra <101543629+Rajendrawardanaputra@users.noreply.github.com>
2022-03-14 14:10:26 +0700

    Add files via upload

---
+++ polimorphisme/vehicle.java
@@ -0,0 +1,11 @@
+public abstract class vehicle {
+    public void function (){
+        System.out.println("Tools transportation");
+    }
+    public void fuel() {
+        System.out.println("fuels");
+    }
+    public abstract void walk();
+
+
+}
