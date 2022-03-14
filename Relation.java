5e3ba1a6815f51ca7ce61288bdadd05b2283c7ab
Rajendrawardanaputra <101543629+Rajendrawardanaputra@users.noreply.github.com>
2022-03-14 13:32:40 +0700

    Add files via upload

---
+++ Relation.java
@@ -0,0 +1,9 @@
+public class Relation {
+    
+
+public interface relation {
+    public boolean isGreater(Object a, Object b);
+    public boolean isLess(Object a, Object b);
+    public boolean isEqual(Object a, Object b);
+}
+}
